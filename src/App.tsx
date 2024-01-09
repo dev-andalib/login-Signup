354fd19e7ba269186a72d6212391882eb1870894
Andalib Iftakher <iftakherandalib@gmail.com>
2024-01-09 23:12:08 +0600

    Login First Part done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 
+import { Grid, GridItem } from '@chakra-ui/react'
 import './App.css'
+import SignIn from './components/SignIn'
 
 function App() {
   
   return (
-      <></>
+      <>
+      <Grid templateAreas={`"form"`} w='100%' h='100vh' bgGradient='linear(to-l, #cbd5ff, #e3e1e3)'>
+        <GridItem area={"form"} padding={10} width={'900px'} height={'550px'} marginX={'auto'} marginY={"250px"} backgroundColor={'#fffeff'} borderRadius={'30px'}> <SignIn></SignIn></GridItem>
+      </Grid>
+      </>
   )
 }
 
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,111 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Grid,
+  GridItem,
+  Input,
+  Square,
+  Text,
+} from "@chakra-ui/react";
+import { IoLogoGoogleplus } from "react-icons/io";
+import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
+
 
 
 const SignIn = () => {
+
+  // const buttonhover = () =>{
+  //   var bgColor = '#585bc9'
+  //   var textColor = '#000000'
+  // }
+
+
   return (
     <>
-      
+      <Grid
+        templateAreas={`"heading" "icons" "form" `}
+        width={"400px"}
+        marginRight={"50px"}
+      >
+        <GridItem
+          area={"heading"}
+          paddingTop={6}
+          fontSize={"40px"}
+          fontWeight={"700"}
+          
+        >
+          <Text textAlign={"center"}>Sign In</Text>
+        </GridItem>
+
+        <GridItem area={"icons"}>
+          <Flex justifyContent={"center"} columnGap={"30px"} marginTop={"10px"}>
+            <Square
+              border={"1px #e7e6e8 solid"}
+              borderRadius={"10px"}
+              padding={"5px"}
+              size={"40px"}
+            >
+              <IoLogoGoogleplus />
+            </Square>
+            <Square
+              border={"1px #e7e6e8 solid"}
+              borderRadius={"10px"}
+              padding={"5px"}
+              size={"40px"}
+            >
+              <FaFacebookF />
+            </Square>
+            <Square
+              border={"1px #e7e6e8 solid"}
+              borderRadius={"10px"}
+              padding={"5px"}
+              size={"40px"}
+            >
+              <FaGithub />
+            </Square>
+            <Square
+              border={"1px #e7e6e8 solid"}
+              borderRadius={"10px"}
+              padding={"5px"}
+              size={"40px"}
+            >
+              <FaLinkedinIn />
+            </Square>
+          </Flex>
+        </GridItem>
+
+        <GridItem area={"form"} paddingTop={"40px"}>
+          <FormControl textAlign={"center"}>
+            <FormLabel textAlign={"center"} color={"#c2c1c2"}>
+              or use your email and pass word{" "}
+            </FormLabel>
+            <Input type="email" placeholder="EMAIL ADDRESS" />
+            <FormLabel textAlign={"center"} color={"#c2c1c2"}>
+              {" "}
+              Now type in you password
+            </FormLabel>
+            <Input type="password" placeholder="PASSWORD" />
+            <FormLabel
+              textAlign={"center"}
+              color={"#c2c1c2"}
+              marginTop={"45px"}
+            >
+              Forgot you password?{" "}
+            </FormLabel>
+            <Button
+              marginTop={"10px"}
+              width={"150px"}
+              color={"#fdebff"}
+              backgroundColor={"#522caa"}
+              
+            >
+              Sign IN
+            </Button>
+          </FormControl>
+        </GridItem>
+      </Grid>
     </>
   );
 };
